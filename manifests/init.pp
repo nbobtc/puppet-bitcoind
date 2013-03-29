f2811511b0f759450fa85dc3dd159643a7e997c4
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2013-03-29 13:57:39 -0500

    made a few updates, more coming later when I get some time

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,6 +9,13 @@ class bitcoind
     $repository = "git://github.com/bitcoin/bitcoin.git"
     $requires   = [
         "git",
+        "build-essential",
+        "libssl-dev",
+        "libdb4.8-dev",
+        "libdb4.8++-dev",
+        "libboost1.48-dev",
+        "libminiupnpc8",
+        "libminiupnpc-dev",
     ]
     $clone_path = "/opt/bitcoin"
     $install_path = "/usr/bin"
@@ -22,22 +29,25 @@ class bitcoind
     }
 
     exec { "git clone bitcoin":
-        path    => "/usr/bin",
-        command => "git clone ${repository} ${clone_path}",
-        creates => "${clone_path}/.git",
+        path      => "/usr/local/bin:/usr/local/sbin:/usr/X11R6/bin:/usr/bin:/usr/sbin:/bin:/sbin:.",
+        command   => "git clone ${repository} ${clone_path}",
+        creates   => "${clone_path}/.git",
+        logoutput => true
     }
 
     exec { "make bitcoin":
-        path    => "/usr/bin",
-        command => "make -f makefile.unix",
-        creates => "${clone_path}/src/bitcoind",
-        cwd     => "${clone_path}/src",
+        path      => "/usr/local/bin:/usr/local/sbin:/usr/X11R6/bin:/usr/bin:/usr/sbin:/bin:/sbin:.",
+        command   => "make -f makefile.unix",
+        creates   => "${clone_path}/src/bitcoind",
+        cwd       => "${clone_path}/src",
+        logoutput => true
     }
 
     exec { "copy binary":
-        path    => "/usr/bin",
-        command => "cp ${clone_path}/src/bitcoind ${install_path}",
-        creates => "${install_path}/bitcoind",
+        path      => "/usr/local/bin:/usr/local/sbin:/usr/X11R6/bin:/usr/bin:/usr/sbin:/bin:/sbin:.",
+        command   => "cp ${clone_path}/src/bitcoind ${install_path}",
+        creates   => "${install_path}/bitcoind",
+        logoutput => true
     }
 
 }
