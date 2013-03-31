bd8d518dc168177ab0fcd1a7ff5a9f588835abf0
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2013-03-31 18:43:14 -0500

    fuck it

---
+++ manifests/ppa.pp
@@ -0,0 +1,45 @@
+####
+#
+# This will install bitcoind with the ppa repository
+#
+class bitcoind::ppa
+{
+    ####
+    #
+    # ppa repository
+    #
+    $repository = "ppa:bitcoin/bitcoin"
+
+    ####
+    #
+    # Add ppa repository
+    #
+    exec { "add-apt-repository ppa:bitcoin/bitcoin":
+        path      => "/usr/local/bin:/usr/local/sbin:/usr/X11R6/bin:/usr/bin:/usr/sbin:/bin:/sbin:.",
+        command   => "add-apt-repository -y ${repository}",
+        logoutput => true,
+        notify    => Exec['update apt-get for bitcoin'],
+    }
+
+    exec { "update apt-get for bitcoin": 
+        path      => "/usr/local/bin:/usr/local/sbin:/usr/X11R6/bin:/usr/bin:/usr/sbin:/bin:/sbin:.",
+        command   => "apt-get update",
+        logoutput => false,
+        require   => Exec['add-apt-repository ppa:bitcoin/bitcoin'],
+    }
+
+    ####
+    #
+    # Instals bitcoind via apt-get
+    #
+    package { "bitcoind":
+        ensure  => present,
+        require => Exec['add-apt-repository ppa:bitcoin/bitcoin'],
+    }
+
+    package { "python-software-properties":
+        ensure  => present,
+        before  => Exec["add-apt-repository ppa:bitcoin/bitcoin"]
+    }
+
+}
