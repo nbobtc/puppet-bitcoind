fc3c7f25e36098ffb3eace1a75e5ef92775a911c
William Moss <willmoss26@gmail.com>
2013-08-31 19:21:33 +0100

    edit source builder

--- manifests/source.pp
+++ manifests/source.pp
@@ -6,6 +6,15 @@ class bitcoind::source
 {
     include bitcoind::params
 
+    notify{"Starting build of bitcoind.... This may take a while": }
+    notify{"NOTE:If you get an error at this stage, enlarge the swap file to 1GB. See - https://bitcointalk.org/index.php?topic=110627.0": }
+    
+    # 1GB SWAPFILE CODE
+    #
+    # sudo dd if=/dev/zero of=/swapfile bs=64M count=16
+    # sudo mkswap /swapfile
+    # sudo swapon /swapfile
+    
     $repository = "git://github.com/bitcoin/bitcoin.git"
     $requires   = [
         "git",
@@ -13,9 +22,11 @@ class bitcoind::source
         "libssl-dev",
         "libdb5.1-dev",
         "libdb++-dev",
-        "libboost1.49-dev",
+        "libboost1.49-all-dev",
+        "libboost1.49-dbg",
+        "libboost-all-dev",
         "libminiupnpc5",
-        "libminiupnpc-dev",
+        "libminiupnpc-dev"
     ]
     $clone_path = "/opt/bitcoin"
     $install_path = "/usr/bin"
